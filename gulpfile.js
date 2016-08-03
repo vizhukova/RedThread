5b336cf671d80ca1c443eb91b88ce573a2644881
Zhukova Veronika <veronikazhukova9@gmail.com>
2016-08-03 19:59:32 +0300

    3.5 slides

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+//var gutil = require('gulp-util');
+//var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+//var sh = require('shelljs');
+//var browserify = require("browserify");
+//var source = require('vinyl-source-stream');
+var rigger = require('gulp-rigger');
+
+var paths = {
+  sass: ['./scss/**.scss']
+};
+
+gulp.task('default', ['sass', 'watch', 'rigger']);
+
+gulp.task('rigger', function () {
+     gulp.src('index.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('sass', function(done) {
+  gulp.src(['./scss/stylesheet.scss'])
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./build/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./build/css/'))
+    .on('end', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch('index.html', ['rigger']);
+  gulp.watch('pages/**.html', ['rigger']);
+});
+
---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    //initialize swiper when document ready  
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      autoHeight: true,
+      mousewheelControl: true
+    })        
+  });
